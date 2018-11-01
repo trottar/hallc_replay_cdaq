1e39ac5298ebfadeb3503d338294cafa83d462d7
trottar <trotta@cua.edu>
2018-11-01 07:32:13 -0400

    Updated root replay for bash file and paths used

--- run_Replay.sh
+++ run_Replay.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 #Which spectrometer 
-spec=$1
-SPEC=$(echo "$spec" | tr '[:lower:]' '[:upper:]')
+#spec=$1
+#SPEC=$(echo "$spec" | tr '[:lower:]' '[:upper:]')
 
 #Input run numbers
-inputFile="../elastics/inputRuns.dat"
+inputFile="../kaonlt_analysis/elastics/inputRuns"
 
 while IFS='' read -r line || [[ -n "$line" ]];
 do
@@ -17,17 +17,19 @@ runNum=$line
 numEvts=-1
 
 #Which scripts to run
-script="replay_elastics_hms.C"
+#script="replay_elastics_hms.C"
+script="replay_production_coin.C"
 
 #which commands to run
-runScript="./hcana -l -q \"SCRIPTS/${SPEC}/PRODUCTION/${script}(${runNum},${numEvts})\""
-rootFile="${spec}_coin_replay_elastics_${runNum}_${numEvts}.root"
+#runScript="./hcana -l -q \"SCRIPTS/${SPEC}/PRODUCTION/${script}(${runNum},${numEvts})\""
+runScript="./hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script}(${runNum},${numEvts})\""
+rootFile="KaonLT_coin_replay_production_${runNum}_${numEvts}.root"
 
 #Excecute 
 {
 
 echo "Running ${script}"
-echo "Getting ${numEvts} number of events for run ${runNum} for ${SPEC}"
+echo "Getting ${numEvts} number of events for run ${runNum}"
 eval ${runScript}
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
