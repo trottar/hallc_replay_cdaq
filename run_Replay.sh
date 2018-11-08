69be168f6086255695d17a687c87c046bcf312e4
trottar <trotta@cua.edu>
2018-11-08 10:49:02 -0500

    Changes to replay shell script from ifarm

--- run_Replay.sh
+++ run_Replay.sh
@@ -1,35 +1,46 @@
 #! /bin/bash
+
 #Which spectrometer 
 #spec=$1
 #SPEC=$(echo "$spec" | tr '[:lower:]' '[:upper:]')
 
-#Input run numbers
-inputFile="../kaonlt_analysis/elastics/inputRuns"
-
-while IFS='' read -r line || [[ -n "$line" ]];
-do
-    echo "Run number read from file: $line"
-
 #Which run
-runNum=$line
+runNum=$1
+
+#Which run type (e.g. elastics, lumiscan)
+rtype=$2
 
 #Number of events
-numEvts=-1
+numEvts=10000
+
+#Initialize enviroment
+#export OSRELEASE="Linux_CentOS7.2.1511-x86_64-gcc5.2.0"
+#source /site/12gev_phys/softenv.sh 2.0
+
+#Initialize hcana
+cd "../hcana/"
+source "../hcana/setup.sh"
+cd "../hallc_replay"
+source "../hallc_replay/setup.sh"
 
 #Which scripts to run
-#script="replay_elastics_hms.C"
-script="replay_production_coin.C"
+if [ ${rtype} = "elastics" ]; then
+    script="replay_production_coin.C"
+    #script="replay_production_coin_hElec_pProt.C"
+fi
+if [ ${rtype} = "lumiscan" ]; then
+    script="replay_luminosity.C"
+fi
 
 #which commands to run
-#runScript="./hcana -l -q \"SCRIPTS/${SPEC}/PRODUCTION/${script}(${runNum},${numEvts})\""
+#runScript="/home/trottar/Analysis/hallc_replay/hcana -l -q \"SCRIPTS/COIN/PRODUCTION/${script}(${runNum},${numEvts})\""
 runScript="./hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script}(${runNum},${numEvts})\""
-rootFile="KaonLT_coin_replay_production_${runNum}_${numEvts}.root"
+#rootFile="KaonLT_coin_replay_production_${runNum}_${numEvts}.root"
 
 #Excecute 
 {
-
-echo "Running ${script}"
 echo "Getting ${numEvts} number of events for run ${runNum}"
+echo "Running ${script}"
 eval ${runScript}
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -37,6 +48,3 @@ echo "END OF RUN ${runNum}"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 }
-
-done < "$inputFile"
-
