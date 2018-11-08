cf779028da594fbc5461a6ee53f439a368f738ae
trottar <trotta@cua.edu>
2018-11-08 16:27:28 -0500

    Fixed issues with replay scripts and rearranged UTIL_KAONLT directory to make it more organized

--- run_Replay.sh
+++ run_Replay.sh
@@ -7,8 +7,9 @@
 #Which run
 runNum=$1
 
-#Which run type (e.g. elastics, lumiscan)
+#Which run type (e.g. elastics, lumiscan,production)
 rtype=$2
+RTYPE=$(echo "$rtype" | tr '[:lower:]' '[:upper:]')
 
 #Number of events
 numEvts=10000
@@ -25,26 +26,60 @@ source "/home/trottar/Analysis/hallc_replay/setup.sh"
 
 #Which scripts to run
 if [ ${rtype} = "elastics" ]; then
-    script="replay_production_coin.C"
-    #script="replay_production_coin_hElec_pProt.C"
+    script_hms="replay_production_hms_coin.C"
+    script_shms="replay_production_shms_coin.C"
+    #which commands to run
+    runScript_hms="/home/trottar/Analysis/hallc_replay/hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script_hms}(${runNum},${numEvts})\""
+    runScript_shms="/home/trottar/Analysis/hallc_replay/hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script_shms}(${runNum},${numEvts})\""
+    #rootFile_hms="KaonLT_coin_replay_production_hms_${runNum}_${numEvts}.root"
+    #rootFile_shms="KaonLT_coin_replay_production_shms_${runNum}_${numEvts}.root"
+    #Excecute 
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RTYPE} CHOSEN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+    echo "Getting ${numEvts} number of events for run ${runNum}"
+    echo ""
+    echo "HMS is being analyzed"
+    echo " "
+    echo "Running ${script_hms}"
+    eval ${runScript_hms}
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "END OF HMS RUN ${runNum}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "SHMS is being analyzed"
+    echo " "
+    echo "Running ${script_shms}"
+    eval ${runScript_shms}
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "END OF SHMS RUN ${runNum}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 fi
 if [ ${rtype} = "lumiscan" ]; then
     script="replay_luminosity.C"
+    #which commands to run
+    runScript="/home/trottar/Analysis/hallc_replay/hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script}(${runNum},${numEvts})\""
+    #rootFile="KaonLT_coin_replay_production_${runNum}_${numEvts}.root"
+    #Excecute 
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RTYPE} CHOSEN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+    echo "Getting ${numEvts} number of events for run ${runNum}"
+    echo "Running ${script}"
+    eval ${runScript}
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "END OF RUN ${runNum}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 fi
-
-#which commands to run
-#runScript="/home/trottar/Analysis/hallc_replay/hcana -l -q \"SCRIPTS/COIN/PRODUCTION/${script}(${runNum},${numEvts})\""
-runScript="/home/trottar/Analysis/hallc_replay/hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script}(${runNum},${numEvts})\""
-#rootFile="KaonLT_coin_replay_production_${runNum}_${numEvts}.root"
-
-#Excecute 
-{
-echo "Getting ${numEvts} number of events for run ${runNum}"
-echo "Running ${script}"
-eval ${runScript}
-
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "END OF RUN ${runNum}"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-
-}
+if [ ${rtype} = "production" ]; then
+    script="replay_production_coin.C"
+    #which commands to run
+    runScript="/home/trottar/Analysis/hallc_replay/hcana -l -q \"UTIL_KAONLT/scripts_Replay/${script}(${runNum},${numEvts})\""
+    #rootFile="KaonLT_Luminosity_coin_replay_production_${runNum}_${numEvts}.root"
+    #Excecute 
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RTYPE} CHOSEN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+    echo "Getting ${numEvts} number of events for run ${runNum}"
+    echo "Running ${script}"
+    eval ${runScript}
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "END OF RUN ${runNum}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
